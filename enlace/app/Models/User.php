f600a864e0e8b6ee0ee42774e515c0c119247cea
Alammduran <alam_duran@hotmail.com>
2021-12-08 22:16:49 -0600

    Validate roles and a little example

--- enlace/app/Models/User.php
+++ enlace/app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function hasRoles(array $roles)
+    {
+        foreach ($roles as $role) {
+            if ($this->role === $role) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- enlace/resources/views/users/users_details.blade.php
+++ enlace/resources/views/users/users_details.blade.php
@@ -1,8 +1,11 @@
 @extends('partials.menu')
 
 @section('content')
-
-<h2>Nombre</h2>
+@if (auth()->user()->hasRoles(['operador']))
+<h2>Nombre del operador</h2>
+@elseif(auth()->user()->hasRoles(['admin']))
+<h2>Nombre del admin</h2>
+@endif
 <p>{{ $user->name }}</p>
 <h2>Correo</h2>
 <p>{{ $user->email }}</p>
