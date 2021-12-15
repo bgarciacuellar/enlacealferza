dd7bb4dd555e0e5080a9b89b785241bbb05c7b20
Alammduran <alam_duran@hotmail.com>
2021-12-14 21:54:03 -0600

    User CRUD 85% (back)

--- enlace/app/Http/Controllers/AdminController.php
+++ enlace/app/Http/Controllers/AdminController.php
@@ -2,20 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use App\Models\AdditionalUserInfo;
 
-class UserController extends Controller
+class AdminController extends Controller
 {
-    public function userList(){
-        $users = User::all();
-        return view('users.users_list', compact('users'));
-    }
+    public function createNewUser(Request $request){
+        $request->validate(
+            [
+                'name' => 'required',
+                'email' => 'required|unique:users,email',
+                'password' => 'nullable',
+                'role' => 'required',
+                'last_name' => 'nullable',
+                'work_area' => 'nullable',
+                'position' => 'nullable',
+                'office' => 'nullable',
+                'company' => 'nullable',
+                'gender' => 'nullable',
+                'birthday' => 'nullable',
+                'municipality' => 'nullable',
+                'civil_status' => 'nullable',
+                'phone_number' => 'nullable',
+                'entry_date' => 'nullable',
+                'departure_dates' => 'nullable',
+            ],
+            [
+                'name.required' => 'Es obligatorio  un nombre',
+                'email.required' => 'Es obligatorio un correo',
+                'email.unique' => 'Correo duplicado',
+                'role.required' => 'Es obligatorio un rol',
+            ]
+        );
 
-    public function userDetails($userId){
-        $user = User::findOrFail($userId);
-        return view('users.users_details', compact('user'));
+        $newUser = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => $request->role,
+            'password' => bcrypt($request->password),
+            
+        ]);
+        AdditionalUserInfo::create(
+            [
+                'user_id' => $newUser->id,
+                'last_name' => $request->last_name,
+                'work_area' => $request->work_area,
+                'position' => $request->position,
+                'office' => $request->office,
+                'company' => $request->company,
+                'gender' => $request->gender,
+                'birthday' => $request->birthday,
+                'municipality' => $request->municipality,
+                'civil_status' => $request->civil_status,
+                'phone_number' => $request->phone_number,
+                'entry_date' => $request->entry_date,
+                'departure_dates' => $request->departure_dates,
+            ]
+        );
+
+        return redirect()->route('users.userList')->with('success', 'Usuario Creado');
     }
 
     public function updateUser(Request $request, $userId){
@@ -79,18 +123,5 @@ class UserController extends Controller
         );
 
         return back()->with('success', 'Usuario Actualizado');
-    }
-
-    public function disableUser(Request $request){
-        $user = User::find($request->user_id);
-        if ($user->is_active) {
-            $user->update([
-                'is_active' => 0
-            ]);
-        }else {
-            return back()->with('error', 'Este usuario ya esta deshabilitado');
-        }
-
-        return back()->with('success', 'Usuario deshabilitado');
     }
 }
--- enlace/app/Models/AdditionalUserInfo.php
+++ enlace/app/Models/AdditionalUserInfo.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class AdditionalUserInfo extends Model
 {
     use HasFactory;
+    
+    protected $guarded = [];
 }
